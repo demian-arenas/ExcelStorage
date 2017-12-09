de9a14ae09917d3c2ba4c7c9b71c77023489d980
Demian Arenas <arenasdemian99@gmail.com>
2017-12-09 00:35:07 -0600

    [add] set firebase config

---
+++ src/config/constants.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: 'AIzaSyBA-PjmUd7PT8aagBNhe_J2sru1Q_FclP4',
+  authDomain: "excelstorage-55567.firebaseapp.com",
+  storageBucket: "gs://excelstorage-55567.appspot.com/"
+};
+
+firebase.initializeApp(config)
+
+export const firebaseAuth = firebase.auth
+export const firebaseStorage = firebase.storage().ref();
