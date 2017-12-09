bfafc032d5129b70ef5015a747fb1c7d8b8f77ea
Demian Arenas <arenasdemian99@gmail.com>
2017-12-09 00:37:06 -0600

    [update] set logic to Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { login } from '../helpers/auth'
 
-class Login extends Component {
-  handleSubmit (event) {
-    event.preventDefault()
-    console.log(event)
-  }
-  render () {
-    return <div id="login-container">
-        <form id="login-box">
-          <p id="login-title">Iniciar sesion</p>
-          <input type="text" id="login-mail" placeholder="Usuario" required />
-          <input type="password" id="login-password" placeholder="Contrasena" required />
-          <input type="submit" id="login-submit" value="Entrar" onClick={this.handleSubmit} />
-        </form>
-      </div>;
+function setErrorMsg(error) {
+  return {
+    loginMessage: error
   }
 }
 
-export default Login
+export default class Login extends Component {
+  state = { loginMessage: null }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    login(this.email.value, this.pw.value)
+      .catch((error) => {
+          this.setState(setErrorMsg('Invalid username/password.'))
+        })
+  }
+  render () {
+    return (
+      <div className="col-sm-6 col-sm-offset-3">
+        <h1> Login </h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Email</label>
+            <input className="form-control" ref={(email) => this.email = email} placeholder="Email"/>
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} />
+          </div>
+          <button type="submit" className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    )
+  }
+}
