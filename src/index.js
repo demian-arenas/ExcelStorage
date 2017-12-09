cf808e4b9ee2b5c5986bc28dea6e217616a2f780
Demian Arenas <arenasdemian99@gmail.com>
2017-12-09 00:38:35 -0600

    [update] set render App

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter }  from "react-router-dom";
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import App from './components';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  <App />,
+  document.getElementById('root')
 );
+
 registerServiceWorker();
