5fb672eecd4fd3cb8f1aace9b651f9e3b2a28e5f
Demian Arenas <arenasdemian99@gmail.com>
2017-12-12 04:39:51 -0600

    [update] export database reference

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,10 +3,13 @@ import * as firebase from "firebase";
 const config = {
   apiKey: 'AIzaSyBA-PjmUd7PT8aagBNhe_J2sru1Q_FclP4',
   authDomain: "excelstorage-55567.firebaseapp.com",
+  databaseURL: "https://excelstorage-55567.firebaseio.com/",
   storageBucket: "gs://excelstorage-55567.appspot.com/"
 };
 
 firebase.initializeApp(config)
 
 export const firebaseAuth = firebase.auth
+export const ref = firebase.database().ref()
+export const docsRef = firebase.database()
 export const firebaseStorage = firebase.storage()
