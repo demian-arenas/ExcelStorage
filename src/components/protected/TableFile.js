8b5e0dfffe01942e60b57d4fa4b10befd01a2ea1
Demian Arenas <arenasdemian99@gmail.com>
2017-12-12 04:38:02 -0600

    [add] create TableFile component in protected folder

---
+++ src/components/protected/TableFile.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class TableFile extends Component {
+  render () {
+    return <div>
+        <table id="Table">
+          <thead id="TableHead">
+            <tr>
+              <th>Nombre</th>
+              <th>Descargar</th>
+              <th>Eliminar</th>
+            </tr>
+          </thead>
+        </table>
+        <div id="Table">
+          <table id="TableBody">
+            <tbody id="TableBodyFiles" />
+          </table>
+        </div>
+      </div>;
+  }
+}
